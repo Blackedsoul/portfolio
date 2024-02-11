a98d6561449368d5d0a0c9f59f792a2b51c97bbd
Blackedsoul <127311296+Blackedsoul@users.noreply.github.com>
2024-02-11 11:04:02 +0100

    update

---
+++ js/gtag.js
@@ -0,0 +1,7 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-0MBYTDD6K1"></script>
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-0MBYTDD6K1');
